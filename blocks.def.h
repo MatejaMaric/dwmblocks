028c389921529399b894fabf03cf3ac335b9c1db
Mateja <mail@matejamaric.com>
2021-06-10 09:58:34 +0200

    Change date format and update signal.

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-  /*Icon    Command          Update Interval    Update Signal*/
-  {"",      "sb_test.sh",    5,                 10},
-  {"",      "date",          1,                 0},
+  /*Icon    Command                       Update Interval    Update Signal*/
+  {"",      "sb_test.sh",                 5,                 10},
+  {"",      "date +'%A %d %b %I:%M %p'",  1,                 20},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
