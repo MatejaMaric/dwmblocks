7e62d12818c5184ebf6cfe3a41d2fe43b81c98cf
Mateja <mail@matejamaric.com>
2021-06-16 21:23:25 +0200

    Added keyboard layout and volume blocks.

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon    Command                       Update Interval    Update Signal*/
-  {"",      "sb_test.sh",                 5,                 10},
-  {"",      "date +'%A %d %b %I:%M %p'",  1,                 20},
+  {" ",     "sb_layout.sh",               0,                 10},
+  {"",      "sb_volume.sh",               10,                11},
+  {"",      "date +'%A %d %b %I:%M %p'",  60,                20},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
